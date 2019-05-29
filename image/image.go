c42df3e39b96dc37ce71f4673791c9a346af5a0b
jfercher <johnathan.fercher@mercadolivre.com>
2019-05-28 23:42:05 -0300

    Extracted module, unit test done

--- image/image.go
+++ image/image.go
@@ -23,13 +23,11 @@ func NewImage(pdf gofpdf.Pdf, math math.Math) Image {
 }
 
 func (i *image) AddFromPath(path string, marginTop float64, indexCol float64, qtdCol float64) {
-	var opt gofpdf.ImageOptions
 	widthPerCol := i.math.GetWidthPerCol(qtdCol)
 
 	left, top, _, _ := i.pdf.GetMargins()
 
-	i.pdf.ImageOptions(path, widthPerCol*indexCol+left, marginTop+top, widthPerCol, 0, false, opt, 0, "")
-
+	i.pdf.ImageOptions(path, widthPerCol*indexCol+left, marginTop+top, widthPerCol, 0, false, gofpdf.ImageOptions{}, 0, "")
 }
 
 func (image) AddFromBase64(base64 string, x, y, width, height float64) {
