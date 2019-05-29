37afa364636b0b253f53c1fc54c3d34b31071b34
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-05-28 23:54:33 -0300

    feature/image-module (#9)
    Done
    * Extract image feature to another module
    
    Why
    * Increase cohesion
    * Improve testing

---
+++ image/image_test.go
@@ -0,0 +1,40 @@
+package image
+
+import (
+	"fmt"
+	"github.com/johnfercher/maroto/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestNewImage(t *testing.T) {
+	image := NewImage(&mocks.Pdf{}, &mocks.Math{})
+
+	assert.NotNil(t, image)
+	assert.Equal(t, fmt.Sprintf("%T", image), "*image.image")
+}
+
+func TestImage_AddFromPath(t *testing.T) {
+	// ARRANGE
+	_pdf := &mocks.Pdf{}
+	_pdf.On("GetMargins").Return(10.0, 10.0, 10.0, 10.0)
+	_pdf.On("ImageOptions", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+
+	_math := &mocks.Math{}
+	_math.On("GetWidthPerCol", mock.Anything).Return(100.0)
+
+	image := NewImage(_pdf, _math)
+
+	// Act
+	image.AddFromPath("AnyPath", 10.0, 1.0, 4.0)
+
+	// Assert
+	_math.AssertNumberOfCalls(t, "GetWidthPerCol", 1)
+	_math.AssertCalled(t, "GetWidthPerCol", 4.0)
+
+	_pdf.AssertNumberOfCalls(t, "GetMargins", 1)
+
+	_pdf.AssertNumberOfCalls(t, "ImageOptions", 1)
+	//_pdf.AssertCalled(t, "ImageOptions", "AnyPath", 110.0, 20.0, 100.0, 0, false, gofpdf.ImageOptions{}, 0, "")
+}
