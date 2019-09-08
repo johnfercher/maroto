3354f7a338b7dccc533396c90b6a2cec10f1a13b
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-09-08 11:42:49 -0300

    doc/improve
    **Description**
    * Improve README.md with use diagram and medium article link
    * Improve documentation with comments in properties

--- properties.go
+++ properties.go
@@ -2,51 +2,85 @@ package maroto
 
 // Proportion represents a proportion from a rectangle, example: 16x9, 4x3...
 type Proportion struct {
-	Width  float64
+	// Width from the rectangle: Barcode, image and etc
+	Width float64
+	// Height from the rectangle: Barcode, image and etc
 	Height float64
 }
 
 // BarcodeProp represents properties from a barcode inside a cell
 type BarcodeProp struct {
-	Left       float64
-	Top        float64
-	Percent    float64
+	// Left is the space between the left cell boundary to the barcode, if center is false
+	Left float64
+	// Top is space between the upper cell limit to the barcode, if center is false
+	Top float64
+	// Percent is how much the barcode will occupy the cell,
+	// ex 100%: The barcode will fulfill the entire cell
+	// ex 50%: The greater side from the barcode will have half the size of the cell
+	Percent float64
+	// Proportion is the proportion between size of the barcode
+	// Ex: 16x9, 4x3...
 	Proportion Proportion
-	Center     bool
+	// Center define that the barcode will be vertically and horizontally centralized
+	Center bool
 }
 
 // RectProp represents properties from a rectangle (Image, QrCode or Barcode) inside a cell
 type RectProp struct {
-	Left    float64
-	Top     float64
+	// Left is the space between the left cell boundary to the rectangle, if center is false
+	Left float64
+	// Top is space between the upper cell limit to the barcode, if center is false
+	Top float64
+	// Percent is how much the rectangle will occupy the cell,
+	// ex 100%: The rectangle will fulfill the entire cell
+	// ex 50%: The greater side from the rectangle will have half the size of the cell
 	Percent float64
-	Center  bool
+	// Center define that the barcode will be vertically and horizontally centralized
+	Center bool
 }
 
 // TextProp represents properties from a Text inside a cell
 type TextProp struct {
-	Top         float64
-	Family      Family
-	Style       Style
-	Size        float64
-	Align       Align
+	// Top is space between the upper cell limit to the barcode, if align is not center
+	Top float64
+	// Family of the text, ex: Arial, helvetica and etc
+	Family Family
+	// Style of the text, ex: Normal, bold and etc
+	Style Style
+	// Size of the text
+	Size float64
+	// Align of the text
+	Align Align
+	// Extrapolate define if the text will automatically add a new line when
+	// text reach the right cell boundary
 	Extrapolate bool
 }
 
 // FontProp represents properties from a text
 type FontProp struct {
+	// Family of the text, ex: Arial, helvetica and etc
 	Family Family
-	Style  Style
-	Size   float64
+	// Style of the text, ex: Normal, bold and etc
+	Style Style
+	// Size of the text
+	Size float64
 }
 
 // TableListProp represents properties from a TableList
 type TableListProp struct {
-	HeaderHeight       float64
-	HeaderProp         FontProp
-	ContentProp        FontProp
-	ContentHeight      float64
-	Align              Align
+	// HeaderHeight is the height of the cell with headers
+	HeaderHeight float64
+	// HeaderProp is the custom properties of the text inside
+	// the headers
+	HeaderProp FontProp
+	// ContentHeight is the height of the cells with contents
+	ContentHeight float64
+	// ContentProp is the custom properties of the text inside
+	// the contents
+	ContentProp FontProp
+	// Align is the align of the text (header and content) inside the columns
+	Align Align
+	// HeaderContentSpace is the space between the header and the contents
 	HeaderContentSpace float64
 }
 
