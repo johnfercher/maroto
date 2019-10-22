73f4c81ea07d126750d5699649c81a381f3f4802
Artem <kolomytsev1996@gmail.com>
2019-10-22 23:41:29 +0300

    allow uncentralized qr/bar-codes

--- pkg/pdf/pdf.go
+++ pkg/pdf/pdf.go
@@ -2,6 +2,7 @@ package pdf
 
 import (
 	"bytes"
+
 	"github.com/johnfercher/maroto/internal"
 	"github.com/johnfercher/maroto/pkg/consts"
 	"github.com/johnfercher/maroto/pkg/props"
@@ -405,11 +406,7 @@ func (s *PdfMaroto) Barcode(code string, prop ...props.Barcode) (err error) {
 	qtdCols := float64(len(s.colsClosures))
 	sumOfyOffsets := s.offsetY + barcodeProp.Top
 
-	if barcodeProp.Center {
-		err = s.Code.AddBar(code, sumOfyOffsets, s.rowColCount, qtdCols, s.rowHeight, barcodeProp.Percent, barcodeProp.Proportion.Height/barcodeProp.Proportion.Width)
-	} else {
-		err = s.Code.AddBar(code, sumOfyOffsets, s.rowColCount, qtdCols, s.rowHeight, barcodeProp.Percent, barcodeProp.Proportion.Height/barcodeProp.Proportion.Width)
-	}
+	err = s.Code.AddBar(code, sumOfyOffsets, s.rowColCount, qtdCols, s.rowHeight, barcodeProp, barcodeProp.Proportion.Height/barcodeProp.Proportion.Width)
 
 	return
 }
@@ -425,12 +422,7 @@ func (s *PdfMaroto) QrCode(code string, prop ...props.Rect) {
 
 	qtdCols := float64(len(s.colsClosures))
 	sumOfyOffsets := s.offsetY + rectProp.Top
-
-	if rectProp.Center {
-		s.Code.AddQr(code, sumOfyOffsets, s.rowColCount, qtdCols, s.rowHeight, rectProp.Percent)
-	} else {
-		s.Code.AddQr(code, sumOfyOffsets, s.rowColCount, qtdCols, s.rowHeight, rectProp.Percent)
-	}
+	s.Code.AddQr(code, sumOfyOffsets, s.rowColCount, qtdCols, s.rowHeight, rectProp)
 }
 
 func (s *PdfMaroto) createColSpace(actualWidthPerCol float64) {
