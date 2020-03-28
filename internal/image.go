2f4e0fde2dc60ac4585fe45c61d1edfa6ca0f934
Johnathan Fercher <johnathanfercher22@gmail.com>
2020-03-27 23:38:18 -0300

    Image using cell

--- internal/image.go
+++ internal/image.go
@@ -12,8 +12,8 @@ import (
 
 // Image is the abstraction which deals of how to add images in a PDF
 type Image interface {
-	AddFromFile(path string, yColOffset float64, xColOffset float64, colWidth float64, colHeight float64, prop props.Rect) (err error)
-	AddFromBase64(b64 string, yColOffset float64, xColOffset float64, colWidth float64, colHeight float64, prop props.Rect, extension consts.Extension) (err error)
+	AddFromFile(path string, cell Cell, prop props.Rect) (err error)
+	AddFromBase64(stringBase64 string, cell Cell, prop props.Rect, extension consts.Extension) (err error)
 }
 
 type image struct {
@@ -30,7 +30,7 @@ func NewImage(pdf gofpdf.Pdf, math Math) *image {
 }
 
 // AddFromFile open an image from disk and add to PDF
-func (s *image) AddFromFile(path string, yColOffset float64, xColOffset float64, colWidth float64, colHeight float64, prop props.Rect) error {
+func (s *image) AddFromFile(path string, cell Cell, prop props.Rect) error {
 	info := s.pdf.RegisterImageOptions(path, gofpdf.ImageOptions{
 		ReadDpi:   false,
 		ImageType: "",
@@ -40,15 +40,15 @@ func (s *image) AddFromFile(path string, yColOffset float64, xColOffset float64,
 		return errors.New("Could not register image options, maybe path/name is wrong")
 	}
 
-	s.addImageToPdf(path, info, yColOffset, colWidth, colHeight, xColOffset, prop)
+	s.addImageToPdf(path, info, cell, prop)
 	return nil
 }
 
 // AddFromBase64 use a base64 string to add to PDF
-func (s *image) AddFromBase64(b64 string, yColOffset float64, xColOffset float64, colWidth float64, colHeight float64, prop props.Rect, extension consts.Extension) error {
+func (s *image) AddFromBase64(stringBase64 string, cell Cell, prop props.Rect, extension consts.Extension) error {
 	imageId, _ := uuid.NewRandom()
 
-	ss, _ := base64.StdEncoding.DecodeString(b64)
+	ss, _ := base64.StdEncoding.DecodeString(stringBase64)
 
 	info := s.pdf.RegisterImageOptionsReader(
 		imageId.String(),
@@ -63,16 +63,16 @@ func (s *image) AddFromBase64(b64 string, yColOffset float64, xColOffset float64
 		return errors.New("Could not register image options, maybe path/name is wrong")
 	}
 
-	s.addImageToPdf(imageId.String(), info, yColOffset, colWidth, colHeight, xColOffset, prop)
+	s.addImageToPdf(imageId.String(), info, cell, prop)
 	return nil
 }
 
-func (s *image) addImageToPdf(imageLabel string, info *gofpdf.ImageInfoType, marginTop, colWidth, colHeight, xColOffset float64, prop props.Rect) {
+func (s *image) addImageToPdf(imageLabel string, info *gofpdf.ImageInfoType, cell Cell, prop props.Rect) {
 	var x, y, w, h float64
 	if prop.Center {
-		x, y, w, h = s.math.GetRectCenterColProperties(info.Width(), info.Height(), colWidth, colHeight, xColOffset, prop.Percent)
+		x, y, w, h = s.math.GetRectCenterColProperties(info.Width(), info.Height(), cell.Width, cell.Height, cell.X, prop.Percent)
 	} else {
-		x, y, w, h = s.math.GetRectNonCenterColProperties(info.Width(), info.Height(), colWidth, colHeight, xColOffset, prop)
+		x, y, w, h = s.math.GetRectNonCenterColProperties(info.Width(), info.Height(), cell.Width, cell.Height, cell.X, prop)
 	}
-	s.pdf.Image(imageLabel, x, y+marginTop, w, h, false, "", 0, "")
+	s.pdf.Image(imageLabel, x, y+cell.Y, w, h, false, "", 0, "")
 }
