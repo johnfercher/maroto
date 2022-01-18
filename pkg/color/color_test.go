75112e4746024d06c5256e2c5ea56fff9079b1f1
Johnathan Fercher <johnathanfercher22@gmail.com>
2022-01-18 19:45:25 -0300

    Allow customize font colors inside TableList (#190)
    * Allow customize font colors inside TableList

--- pkg/color/color_test.go
+++ pkg/color/color_test.go
@@ -16,6 +16,16 @@ func TestNewWhite(t *testing.T) {
 	assert.Equal(t, 255, white.Blue)
 }
 
+func TestNewBlack(t *testing.T) {
+	// Act
+	black := color.NewBlack()
+
+	// Assert
+	assert.Equal(t, 0, black.Red)
+	assert.Equal(t, 0, black.Green)
+	assert.Equal(t, 0, black.Blue)
+}
+
 func TestColor_IsWhite(t *testing.T) {
 	// Act
 	white := color.NewWhite()
