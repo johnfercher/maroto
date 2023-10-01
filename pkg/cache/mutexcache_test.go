4f631503a684b13af60587cb40f35ce46a701102
John Fercher <johnathanfercher22@gmail.com>
2023-10-01 20:55:45 -0300

    mutexcache test

--- pkg/cache/mutexcache_test.go
+++ pkg/cache/mutexcache_test.go
@@ -1,8 +1,13 @@
 package cache_test
 
 import (
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/johnfercher/maroto/v2/mocks"
+	"github.com/johnfercher/maroto/v2/pkg/consts/extension"
+	"github.com/johnfercher/maroto/v2/pkg/core/entity"
 
 	"github.com/johnfercher/maroto/v2/pkg/cache"
 	"github.com/stretchr/testify/assert"
@@ -15,4 +20,61 @@ func TestNewMutexDecorator(t *testing.T) {
 	// Assert
 	assert.NotNil(t, sut)
 	assert.Equal(t, "*cache.mutexCache", fmt.Sprintf("%T", sut))
+}
+
+func TestMutexCache_AddImage(t *testing.T) {
+	// Arrange
+	value := "value1"
+	img := &entity.Image{}
+
+	innerMock := &mocks.Cache{}
+	innerMock.EXPECT().AddImage(value, img)
+
+	sut := cache.NewMutexDecorator(innerMock)
+
+	// Act
+	sut.AddImage(value, img)
+
+	// Assert
+	innerMock.AssertNumberOfCalls(t, "AddImage", 1)
+}
+
+func TestMutexCache_GetImage(t *testing.T) {
+	// Arrange
+	value := "value2"
+	ext := extension.Jpg
+	imgToReturn := &entity.Image{}
+	errToReturn := errors.New("any error")
+
+	innerMock := &mocks.Cache{}
+	innerMock.EXPECT().GetImage(value, ext).Return(imgToReturn, errToReturn)
+
+	sut := cache.NewMutexDecorator(innerMock)
+
+	// Act
+	img, err := sut.GetImage(value, ext)
+
+	// Assert
+	assert.Equal(t, imgToReturn, img)
+	assert.Equal(t, errToReturn, err)
+	innerMock.AssertNumberOfCalls(t, "GetImage", 1)
+}
+
+func TestMutexCache_LoadImage(t *testing.T) {
+	// Arrange
+	value := "value3"
+	ext := extension.Jpg
+	errToReturn := errors.New("any error")
+
+	innerMock := &mocks.Cache{}
+	innerMock.EXPECT().LoadImage(value, ext).Return(errToReturn)
+
+	sut := cache.NewMutexDecorator(innerMock)
+
+	// Act
+	err := sut.LoadImage(value, ext)
+
+	// Assert
+	assert.Equal(t, errToReturn, err)
+	innerMock.AssertNumberOfCalls(t, "LoadImage", 1)
 }
