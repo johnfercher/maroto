f627cdefc88dd32f6268dac97aa820bd5b751dad
John Fercher <johnathanfercher22@gmail.com>
2023-10-10 12:55:57 -0300

    Improve col tests

--- pkg/components/col/col_test.go
+++ pkg/components/col/col_test.go
@@ -1,7 +1,13 @@
 package col_test
 
 import (
+	"github.com/johnfercher/maroto/v2/internal/fixture"
+	"github.com/johnfercher/maroto/v2/mocks"
+	"github.com/johnfercher/maroto/v2/pkg/components/code"
+	"github.com/johnfercher/maroto/v2/pkg/core/entity"
+	"github.com/johnfercher/maroto/v2/pkg/props"
 	"github.com/johnfercher/maroto/v2/pkg/test"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/johnfercher/maroto/v2/pkg/components/col"
@@ -15,16 +21,23 @@ func TestNew(t *testing.T) {
 		// Assert
 		test.New(t).Assert(c.GetStructure()).Equals("components/cols/new_zero_size.json")
 	})
-	t.Run("when size is not defined, should use is as max", func(t *testing.T) {
+	t.Run("when size is defined, should use not use max", func(t *testing.T) {
 		// Act
 		c := col.New(12)
 
 		// Assert
 		test.New(t).Assert(c.GetStructure()).Equals("components/cols/new_defined_size.json")
 	})
+	t.Run("when has component, should retrieve components", func(t *testing.T) {
+		// Act
+		c := col.New(12).Add(code.NewQr("code"))
+
+		// Assert
+		test.New(t).Assert(c.GetStructure()).Equals("components/cols/new_with_components.json")
+	})
 }
 
-/*func TestCol_GetSize(t *testing.T) {
+func TestCol_GetSize(t *testing.T) {
 	t.Run("when size defined in creation, should use it", func(t *testing.T) {
 		// Arrange
 		c := col.New(12)
@@ -47,4 +60,31 @@ func TestNew(t *testing.T) {
 		assert.Equal(t, 14, size)
 	})
 }
-*/
+
+func TestCol_Render(t *testing.T) {
+	t.Run("should call provider correctly", func(t *testing.T) {
+		// Arrange
+		cfg := &entity.Config{}
+		cell := fixture.CellEntity()
+		style := &props.Cell{}
+
+		provider := &mocks.Provider{}
+		provider.EXPECT().CreateCol(cell.Width, cell.Height, cfg, style)
+
+		component := &mocks.Component{}
+		component.EXPECT().Render(provider, &cell)
+		component.EXPECT().SetConfig(cfg)
+
+		sut := col.New(12).Add(component)
+		sut.WithStyle(style)
+		sut.SetConfig(cfg)
+
+		// Act
+		sut.Render(provider, cell, true)
+
+		// Assert
+		provider.AssertNumberOfCalls(t, "CreateCol", 1)
+		component.AssertNumberOfCalls(t, "Render", 1)
+		component.AssertNumberOfCalls(t, "SetConfig", 1)
+	})
+}
