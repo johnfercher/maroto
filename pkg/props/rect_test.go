3400cf80d68b27491e64c51f46ad5f2d493355e8
John Fercher <4752636+johnfercher@users.noreply.github.com>
2023-10-10 21:26:46 -0300

    V2 test rect prop (#344)

--- pkg/props/rect_test.go
+++ pkg/props/rect_test.go
@@ -3,68 +3,77 @@ package props_test
 import (
 	"testing"
 
+	"github.com/johnfercher/maroto/v2/internal/fixture"
+
 	"github.com/johnfercher/maroto/v2/pkg/props"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRectProp_MakeValid(t *testing.T) {
-	cases := []struct {
-		name     string
-		rectProp props.Rect
-		assert   func(t *testing.T, prop props.Rect)
-	}{
-		{
-			"When percent is less than zero, should become 100",
-			props.Rect{
-				Percent: -2,
-			},
-			func(t *testing.T, prop props.Rect) {
-				assert.Equal(t, prop.Percent, 100.0)
-			},
-		},
-		{
-			"When percent is greater than 100, should become 100",
-			props.Rect{
-				Percent: 102,
-			},
-			func(t *testing.T, prop props.Rect) {
-				assert.Equal(t, prop.Percent, 100.0)
-			},
-		},
-		{
-			"When is center, top and left should become 0",
-			props.Rect{
-				Center: true,
-				Top:    5,
-				Left:   5,
-			},
-			func(t *testing.T, prop props.Rect) {
-				assert.Equal(t, prop.Top, 0.0)
-				assert.Equal(t, prop.Left, 0.0)
-			},
-		},
-		{
-			"When left is less than 0, should become 0",
-			props.Rect{
-				Left: -5,
-			},
-			func(t *testing.T, prop props.Rect) {
-				assert.Equal(t, prop.Left, 0.0)
-			},
-		},
-		{
-			"When top is less than 0, should become 0",
-			props.Rect{
-				Top: -5,
-			},
-			func(t *testing.T, prop props.Rect) {
-				assert.Equal(t, prop.Top, 0.0)
-			},
-		},
-	}
+func TestRect_MakeValid(t *testing.T) {
+	t.Run("when percent is less than zero, should become 100", func(t *testing.T) {
+		// Arrange
+		prop := props.Rect{Percent: -2}
 
-	for _, c := range cases {
-		c.rectProp.MakeValid()
-		c.assert(t, c.rectProp)
-	}
+		// Act
+		prop.MakeValid()
+
+		// Assert
+		assert.Equal(t, prop.Percent, 100.0)
+	})
+	t.Run("when percent is greater than 100, should become 100", func(t *testing.T) {
+		// Arrange
+		prop := props.Rect{Percent: 102}
+
+		// Act
+		prop.MakeValid()
+
+		// Assert
+		assert.Equal(t, prop.Percent, 100.0)
+	})
+	t.Run("when is center, top and left should become 0", func(t *testing.T) {
+		// Arrange
+		prop := props.Rect{Center: true, Top: 5, Left: 5}
+
+		// Act
+		prop.MakeValid()
+
+		// Assert
+		assert.Equal(t, prop.Top, 0.0)
+		assert.Equal(t, prop.Left, 0.0)
+	})
+	t.Run("when left is less than 0, should become 0", func(t *testing.T) {
+		// Arrange
+		prop := props.Rect{Left: -5}
+
+		// Act
+		prop.MakeValid()
+
+		// Assert
+		assert.Equal(t, prop.Left, 0.0)
+	})
+	t.Run("when top is less than 0, should become 0", func(t *testing.T) {
+		// Arrange
+		prop := props.Rect{Top: -5}
+
+		// Act
+		prop.MakeValid()
+
+		// Assert
+		assert.Equal(t, prop.Top, 0.0)
+	})
+}
+
+func TestRect_ToMap(t *testing.T) {
+	// Arrange
+	sut := fixture.RectProp()
+	sut.Center = true
+
+	// Act
+	m := sut.ToMap()
+
+	// Assert
+	assert.Equal(t, 10.0, m["prop_left"])
+	assert.Equal(t, 10.0, m["prop_top"])
+	assert.Equal(t, 98.0, m["prop_percent"])
+	assert.Equal(t, true, m["prop_center"])
 }
