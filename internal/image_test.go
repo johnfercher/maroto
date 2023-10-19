6a09c2dec8de7acb734221f820505b9477af1a83
John Fercher <4752636+johnfercher@users.noreply.github.com>
2023-10-18 23:34:14 -0300

    Improve unit tests (#350)

--- internal/image_test.go
+++ internal/image_test.go
@@ -1,8 +1,14 @@
 package internal_test
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
+
+	"github.com/johnfercher/maroto/v2/internal/fixture"
+	"github.com/johnfercher/maroto/v2/internal/math"
+	"github.com/jung-kurt/gofpdf"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/johnfercher/maroto/v2/internal"
 	"github.com/johnfercher/maroto/v2/mocks"
@@ -16,348 +22,78 @@ func TestNewImage(t *testing.T) {
 	assert.Equal(t, fmt.Sprintf("%T", image), "*internal.image")
 }
 
-/*func TestImage_AddFromFile(t *testing.T) {
-	cases := []struct {
-		name            string
-		Fpdf            func() *mocks.Fpdf
-		math            func() *mocks.Math
-		assertFpdfCalls func(t *testing.T, Fpdf *mocks.Fpdf)
-		assertMathCalls func(t *testing.T, Fpdf *mocks.Math)
-		assertErr       func(t *testing.T, err error)
-		props           props.Rect
-	}{
-		{
-			"When cannot load image",
-			func() *mocks.Fpdf {
-				Fpdf := &mocks.Fpdf{}
-				Fpdf.On("RegisterImageOptions", mock.Anything, mock.Anything).Return(nil)
-				Fpdf.On("Image", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything)
-				return Fpdf
-			},
-			func() *mocks.Math {
-				math := &mocks.Math{}
-				math.On("GetInnerCenterCell", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-					Return(100.0, 20.0, 33.0, 0.0)
-				return math
-			},
-			func(t *testing.T, Fpdf *mocks.Fpdf) {
-				Fpdf.AssertNumberOfCalls(t, "Image", 0)
-
-				Fpdf.AssertNumberOfCalls(t, "RegisterImageOptions", 1)
-				Fpdf.AssertCalled(t, "RegisterImageOptions", "AnyPath", gofpdf.ImageOptions{
-					ReadDpi:   false,
-					ImageType: "",
-				})
-			},
-			func(t *testing.T, math *mocks.Math) {
-				math.AssertNumberOfCalls(t, "GetInnerCenterCell", 0)
-			},
-			func(t *testing.T, err error) {
-				assert.NotNil(t, err)
-			},
-			props.Rect{Center: true, Percent: 100},
-		},
-		// nolint:dupl // better this way
-		{
-			"When Image has width greater than height",
-			func() *mocks.Fpdf {
-				Fpdf := &mocks.Fpdf{}
-				Fpdf.On("RegisterImageOptions", mock.Anything, mock.Anything).Return(widthGreaterThanHeightImageInfo())
-				Fpdf.On("Image", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything)
-				return Fpdf
-			},
-			func() *mocks.Math {
-				math := &mocks.Math{}
-				math.On("GetInnerCenterCell", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-					Return(100.0, 20.0, 33.0, 0.0)
-				return math
-			},
-			func(t *testing.T, Fpdf *mocks.Fpdf) {
-				Fpdf.AssertNumberOfCalls(t, "Image", 0)
-				Fpdf.AssertCalled(t, "Image", "", 100, 30, 33, 0)
-
-				Fpdf.AssertNumberOfCalls(t, "RegisterImageOptions", 1)
-				Fpdf.AssertCalled(t, "RegisterImageOptions", "AnyPath", gofpdf.ImageOptions{
-					ReadDpi:   false,
-					ImageType: "",
-				})
-			},
-			func(t *testing.T, math *mocks.Math) {
-				math.AssertNumberOfCalls(t, "GetInnerCenterCell", 0)
-				math.AssertCalled(t, "GetInnerCenterCell", 88, 119, 4, 5, 1, 100)
-			},
-			func(t *testing.T, err error) {
-				assert.Nil(t, err)
-			},
-			props.Rect{Center: true, Percent: 100},
-		},
-		// nolint:dupl // better this way
-		{
-			"When Image has height greater than width",
-			func() *mocks.Fpdf {
-				Fpdf := &mocks.Fpdf{}
-				Fpdf.On("RegisterImageOptions", mock.Anything, mock.Anything).Return(heightGreaterThanWidthImageInfo())
-				Fpdf.On("Image", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything)
-				return Fpdf
-			},
-			func() *mocks.Math {
-				math := &mocks.Math{}
-				math.On("GetInnerCenterCell", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-					Return(100.0, 20.0, 33.0, 0.0)
-				return math
-			},
-			func(t *testing.T, Fpdf *mocks.Fpdf) {
-				Fpdf.AssertNumberOfCalls(t, "Image", 1)
-				Fpdf.AssertCalled(t, "Image", "", 100, 30, 33, 0)
-
-				Fpdf.AssertNumberOfCalls(t, "RegisterImageOptions", 1)
-				Fpdf.AssertCalled(t, "RegisterImageOptions", "AnyPath", gofpdf.ImageOptions{
-					ReadDpi:   false,
-					ImageType: "",
-				})
-			},
-			func(t *testing.T, math *mocks.Math) {
-				math.AssertNumberOfCalls(t, "GetInnerCenterCell", 1)
-				math.AssertCalled(t, "GetInnerCenterCell", 661, 521, 4, 5, 1, 100)
-			},
-			func(t *testing.T, err error) {
-				assert.Nil(t, err)
-			},
-			props.Rect{Center: true, Percent: 100},
-		},
-		{
-			"When Image must not be centered",
-			func() *mocks.Fpdf {
-				Fpdf := &mocks.Fpdf{}
-				Fpdf.On("RegisterImageOptions", mock.Anything, mock.Anything).Return(nonCenteredImageInfo())
-				Fpdf.On("Image", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything, mock.Anything)
-				return Fpdf
-			},
-			func() *mocks.Math {
-				math := &mocks.Math{}
-				math.On("GetInnerNonCenterCell", mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything).Return(100.0, 20.0, 33.0, 0.0)
-				return math
-			},
-			func(t *testing.T, Fpdf *mocks.Fpdf) {
-				Fpdf.AssertNumberOfCalls(t, "Image", 1)
-				Fpdf.AssertCalled(t, "Image", "", 100, 30, 33, 0)
-
-				Fpdf.AssertNumberOfCalls(t, "RegisterImageOptions", 1)
-				Fpdf.AssertCalled(t, "RegisterImageOptions", "AnyPath", gofpdf.ImageOptions{
-					ReadDpi:   false,
-					ImageType: "",
-				})
-			},
-			func(t *testing.T, math *mocks.Math) {
-				math.AssertNumberOfCalls(t, "GetInnerNonCenterCell", 1)
-				math.AssertCalled(t, "GetInnerNonCenterCell", 661, 521, 4, 5, 1, props.Rect{Center: false, Percent: 100})
-			},
-			func(t *testing.T, err error) {
-				assert.Nil(t, err)
-			},
-			props.Rect{Center: false, Percent: 100},
-		},
-	}
-
-	for _, c := range cases {
+func TestImage_Add(t *testing.T) {
+	t.Run("when RegisterImageOptionsReader return nil, should return error", func(t *testing.T) {
 		// Arrange
-		Fpdf := c.Fpdf()
-		math := c.math()
-
-		image := internal.NewImage(Fpdf, math)
-		cell := internal.Cell{
-			X:      1.0,
-			Y:      10.0,
-			Width:  4.0,
-			Height: 5.0,
+		cell := fixture.CellEntity()
+		margins := fixture.MarginsEntity()
+		rect := fixture.RectProp()
+		img := fixture.ImageEntity()
+		options := gofpdf.ImageOptions{
+			ReadDpi:   false,
+			ImageType: string(img.Extension),
 		}
 
+		pdf := &mocks.Fpdf{}
+		pdf.EXPECT().RegisterImageOptionsReader(mock.Anything, options, bytes.NewReader(img.Bytes)).Return(nil)
+
+		image := internal.NewImage(pdf, &mocks.Math{})
+
 		// Act
-		err := image.AddFromFile("AnyPath", cell, c.props)
+		err := image.Add(&img, &cell, &margins, &rect, img.Extension, true)
 
 		// Assert
-		c.assertFpdfCalls(t, Fpdf)
-		c.assertMathCalls(t, math)
-		c.assertErr(t, err)
-	}
-}
-
-func TestImage_AddFromBase64(t *testing.T) {
-	cases := []struct {
-		name            string
-		Fpdf            func() *mocks.Fpdf
-		math            func() *mocks.Math
-		assertFpdfCalls func(t *testing.T, Fpdf *mocks.Fpdf)
-		assertMathCalls func(t *testing.T, Fpdf *mocks.Math)
-		assertErr       func(t *testing.T, err error)
-	}{
-		{
-			"When cannot RegisterImageOptionsReader",
-			func() *mocks.Fpdf {
-				Fpdf := &mocks.Fpdf{}
-				Fpdf.On("RegisterImageOptionsReader", mock.Anything, mock.Anything, mock.Anything).Return(nil)
-				Fpdf.On("Image", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything, mock.Anything)
-				return Fpdf
-			},
-			func() *mocks.Math {
-				math := &mocks.Math{}
-				math.On("GetInnerCenterCell", mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything).Return(100.0, 20.0, 33.0, 0.0)
-				return math
-			},
-			func(t *testing.T, Fpdf *mocks.Fpdf) {
-				Fpdf.AssertNumberOfCalls(t, "Image", 0)
-				Fpdf.AssertNumberOfCalls(t, "RegisterImageOptionsReader", 1)
-				Fpdf.AssertCalled(t, "RegisterImageOptionsReader", "", gofpdf.ImageOptions{
-					ReadDpi:   false,
-					ImageType: string(consts.Jpg),
-				},
-					"")
-			},
-			func(t *testing.T, math *mocks.Math) {
-				math.AssertNumberOfCalls(t, "GetInnerCenterCell", 0)
-			},
-			func(t *testing.T, err error) {
-				assert.NotNil(t, err)
-			},
-		},
-		// nolint:dupl // better this way
-		{
-			"When ImageHelper has width greater than height",
-			func() *mocks.Fpdf {
-				Fpdf := &mocks.Fpdf{}
-				Fpdf.On("RegisterImageOptionsReader", mock.Anything, mock.Anything, mock.Anything).Return(widthGreaterThanHeightImageInfo())
-				Fpdf.On("Image", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything)
-				return Fpdf
-			},
-			func() *mocks.Math {
-				math := &mocks.Math{}
-				math.On("GetInnerCenterCell", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-					Return(100.0, 20.0, 33.0, 0.0)
-				return math
-			},
-			func(t *testing.T, Fpdf *mocks.Fpdf) {
-				Fpdf.AssertNumberOfCalls(t, "Image", 1)
-				Fpdf.AssertCalled(t, "Image", "", 100, 30, 33, 0)
-
-				Fpdf.AssertNumberOfCalls(t, "RegisterImageOptionsReader", 1)
-				Fpdf.AssertCalled(t, "RegisterImageOptionsReader", "", gofpdf.ImageOptions{
-					ReadDpi:   false,
-					ImageType: string(consts.Jpg),
-				},
-					"")
-			},
-			func(t *testing.T, math *mocks.Math) {
-				math.AssertNumberOfCalls(t, "GetInnerCenterCell", 1)
-				math.AssertCalled(t, "GetInnerCenterCell", 88, 119, 4, 5, 1, 100)
-			},
-			func(t *testing.T, err error) {
-				assert.Nil(t, err)
-			},
-		},
-		// nolint:dupl // better this way
-		{
-			"When ImageHelper has height greater than width",
-			func() *mocks.Fpdf {
-				Fpdf := &mocks.Fpdf{}
-				Fpdf.On("RegisterImageOptionsReader", mock.Anything, mock.Anything, mock.Anything).Return(heightGreaterThanWidthImageInfo())
-				Fpdf.On("Image", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything, mock.Anything)
-				return Fpdf
-			},
-			func() *mocks.Math {
-				math := &mocks.Math{}
-				math.On("GetInnerCenterCell", mock.Anything, mock.Anything, mock.Anything, mock.Anything,
-					mock.Anything, mock.Anything).Return(100.0, 20.0, 33.0, 0.0)
-				return math
-			},
-			func(t *testing.T, Fpdf *mocks.Fpdf) {
-				Fpdf.AssertNumberOfCalls(t, "Image", 1)
-				Fpdf.AssertCalled(t, "Image", "", 100, 30, 33, 0)
-
-				Fpdf.AssertNumberOfCalls(t, "RegisterImageOptionsReader", 1)
-				Fpdf.AssertCalled(t, "RegisterImageOptionsReader", "", gofpdf.ImageOptions{
-					ReadDpi:   false,
-					ImageType: string(consts.Jpg),
-				},
-					"")
-			},
-			func(t *testing.T, math *mocks.Math) {
-				math.AssertNumberOfCalls(t, "GetInnerCenterCell", 1)
-				math.AssertCalled(t, "GetInnerCenterCell", 661, 521, 4, 5, 1, 100)
-			},
-			func(t *testing.T, err error) {
-				assert.Nil(t, err)
-			},
-		},
-	}
-
-	for _, c := range cases {
+		assert.NotNil(t, err)
+	})
+	t.Run("when prop is not center, should work properly", func(t *testing.T) {
 		// Arrange
-		Fpdf := c.Fpdf()
-		math := c.math()
-
-		image := internal.NewImage(Fpdf, math)
-		base64 := getBase64String()
-		cell := internal.Cell{
-			X:      1.0,
-			Y:      10.0,
-			Width:  4.0,
-			Height: 5.0,
+		cell := fixture.CellEntity()
+		margins := fixture.MarginsEntity()
+		rect := fixture.RectProp()
+		img := fixture.ImageEntity()
+		options := gofpdf.ImageOptions{
+			ReadDpi:   false,
+			ImageType: string(img.Extension),
 		}
 
+		pdf := &mocks.Fpdf{}
+		pdf.EXPECT().RegisterImageOptionsReader(mock.Anything, options, bytes.NewReader(img.Bytes)).Return(&gofpdf.ImageInfoType{})
+		pdf.EXPECT().Image(mock.Anything, 30.0, 35.0, 98.0, mock.Anything, true, "", 0, "")
+
+		m := math.New()
+
+		image := internal.NewImage(pdf, m)
+
 		// Act
-		err := image.AddFromBase64(base64, cell, props.Rect{Center: true, Percent: 100}, consts.Jpg)
+		err := image.Add(&img, &cell, &margins, &rect, img.Extension, true)
 
 		// Assert
-		c.assertFpdfCalls(t, Fpdf)
-		c.assertMathCalls(t, math)
-		c.assertErr(t, err)
-	}
-}
-
-func heightGreaterThanWidthImageInfo() *gofpdf.ImageInfoType {
-	trueFpdf := gofpdf.New("P", "mm", "A4", "")
-
-	info := trueFpdf.RegisterImageOptions("assets/images/biplane.jpg", gofpdf.ImageOptions{
-		ReadDpi:   false,
-		ImageType: "",
+		assert.Nil(t, err)
 	})
+	t.Run("when prop is center, should work properly", func(t *testing.T) {
+		// Arrange
+		cell := fixture.CellEntity()
+		margins := fixture.MarginsEntity()
+		rect := fixture.RectProp()
+		rect.Center = true
+		img := fixture.ImageEntity()
+		options := gofpdf.ImageOptions{
+			ReadDpi:   false,
+			ImageType: string(img.Extension),
+		}
 
-	return info
-}
+		pdf := &mocks.Fpdf{}
+		pdf.EXPECT().RegisterImageOptionsReader(mock.Anything, options, bytes.NewReader(img.Bytes)).Return(&gofpdf.ImageInfoType{})
+		pdf.EXPECT().Image(mock.Anything, 21.0, mock.Anything, 98.0, mock.Anything, true, "", 0, "")
 
-func widthGreaterThanHeightImageInfo() *gofpdf.ImageInfoType {
-	trueFpdf := gofpdf.New("P", "mm", "A4", "")
-	wrapper := fpdf.NewWrapper(trueFpdf)
+		m := math.New()
 
-	info := wrapper.RegisterImageOptions("assets/images/frontpage.png", gofpdf.ImageOptions{
-		ReadDpi:   false,
-		ImageType: "",
+		image := internal.NewImage(pdf, m)
+
+		// Act
+		err := image.Add(&img, &cell, &margins, &rect, img.Extension, true)
+
+		// Assert
+		assert.Nil(t, err)
 	})
-
-	return info
 }
-
-func nonCenteredImageInfo() *gofpdf.ImageInfoType {
-	trueFpdf := gofpdf.New("P", "mm", "A4", "")
-
-	info := trueFpdf.RegisterImageOptions("assets/images/biplane.jpg", gofpdf.ImageOptions{
-		ReadDpi:   false,
-		ImageType: "",
-	})
-
-	return info
-}
-
-func getBase64String() string {
-	byteSlices, _ := ioutil.ReadFile("assets/images/frontpage.png")
-	return base64.StdEncoding.EncodeToString(byteSlices)
-}*/
