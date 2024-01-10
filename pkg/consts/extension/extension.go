3c4ddc0accb8cdc570c471f88a576254ced4993b
John Fercher <johnathanfercher22@gmail.com>
2024-01-09 21:41:36 -0300

    Fix jpeg image loading

--- pkg/consts/extension/extension.go
+++ pkg/consts/extension/extension.go
@@ -6,10 +6,12 @@ type Type string
 const (
 	// Jpg represents a jpg extension.
 	Jpg Type = "jpg"
+	// Jpeg represents a jpeg extension.
+	Jpeg Type = "jpeg"
 	// Png represents a png extension.
 	Png Type = "png"
 )
 
 func (t Type) IsValid() bool {
-	return t == Jpg || t == Png
+	return t == Jpg || t == Jpeg || t == Png
 }
