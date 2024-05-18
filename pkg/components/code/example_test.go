26d19aa260e1053d1d0cc41df77a1af2fcef9e2a
John Fercher <johnathanfercher22@gmail.com>
2024-05-18 10:42:17 -0300

    Improve docs
    * cell props
    * page props
    * row component
    * add code examples
    * add row examples

--- pkg/components/code/example_test.go
+++ pkg/components/code/example_test.go
@@ -8,32 +8,95 @@ import (
 	"github.com/johnfercher/maroto/v2/pkg/components/code"
 )
 
-// ExampleNewBar demonstrates how to generate a barcode and add it to maroto
+// ExampleNewBar demonstrates how to generate a barcode and add it to maroto.
 func ExampleNewBar() {
-	mrt := maroto.New()
-	m := maroto.NewMetricsDecorator(mrt)
+	m := maroto.New()
 
-	m.AddRow(10, col.New(6).Add(code.NewBar("123456789", props.Barcode{Percent: 70.5})))
+	barCode := code.NewBar("123456789", props.Barcode{Percent: 70.5})
+	col := col.New(6).Add(barCode)
+	m.AddRow(10, col)
 
 	// generate document
 }
 
-// ExampleNewBar demonstrates how to generate a column with a barcode and add it to maroto
+// ExampleNewBarCol demonstrates how to generate a column with a barcode and add it to maroto.
 func ExampleNewBarCol() {
-	mrt := maroto.New()
-	m := maroto.NewMetricsDecorator(mrt)
+	m := maroto.New()
 
-	m.AddRow(10, code.NewBarCol(6, "123456", props.Barcode{Percent: 70.5}))
+	barCodeCol := code.NewBarCol(6, "123456", props.Barcode{Percent: 70.5})
+	m.AddRow(10, barCodeCol)
 
 	// generate document
 }
 
-// ExampleNewBarRow demonstrates how to generate a row with a barcode and add it to maroto
+// ExampleNewBarRow demonstrates how to generate a row with a barcode and add it to maroto.
 func ExampleNewBarRow() {
-	mrt := maroto.New()
-	m := maroto.NewMetricsDecorator(mrt)
+	m := maroto.New()
 
-	m.AddRows(code.NewBarRow(10, "123456789", props.Barcode{Percent: 70.5}))
+	barCodeRow := code.NewBarRow(10, "123456789", props.Barcode{Percent: 70.5})
+	m.AddRows(barCodeRow)
+
+	// generate document
+}
+
+// ExampleNewQr demonstrates how to generate a qrcode and add it to maroto.
+func ExampleNewQr() {
+	m := maroto.New()
+
+	qrCode := code.NewQr("123456789", props.Rect{Percent: 70.5})
+	col := col.New(6).Add(qrCode)
+	m.AddRow(10, col)
+
+	// generate document
+}
+
+// ExampleNewQrCol demonstrates how to generate a column with a qrcode and add it to maroto.
+func ExampleNewQrCol() {
+	m := maroto.New()
+
+	qrCodeCol := code.NewQrCol(12, "123456789", props.Rect{Percent: 70.5})
+	m.AddRow(10, qrCodeCol)
+
+	// generate document
+}
+
+// ExampleNewQrRow demonstrates how to generate a row with a qrcode and add it to maroto.
+func ExampleNewQrRow() {
+	m := maroto.New()
+
+	qrCodeRow := code.NewQrRow(10, "123456789", props.Rect{Percent: 70.5})
+	m.AddRows(qrCodeRow)
+
+	// generate document
+}
+
+// ExampleNewMatrix demonstrates how to generate a matrixcode and add it to maroto.
+func ExampleNewMatrix() {
+	m := maroto.New()
+
+	matrixCode := code.NewMatrix("123456789", props.Rect{Percent: 70.5})
+	col := col.New(6).Add(matrixCode)
+	m.AddRow(10, col)
+
+	// generate document
+}
+
+// ExampleNewMatrixCol demonstrates how to generate a column with a matrixcode and add it to maroto.
+func ExampleNewMatrixCol() {
+	m := maroto.New()
+
+	matrixCodeCol := code.NewMatrixCol(12, "123456789", props.Rect{Percent: 70.5})
+	m.AddRow(10, matrixCodeCol)
+
+	// generate document
+}
+
+// ExampleNewMatrixRow demonstrates how to generate a row with a matrixcode and add it to maroto.
+func ExampleNewMatrixRow() {
+	m := maroto.New()
+
+	matrixCodeRow := code.NewMatrixRow(10, "123456789", props.Rect{Percent: 70.5})
+	m.AddRows(matrixCodeRow)
 
 	// generate document
 }
--- pkg/props/page.go
+++ pkg/props/page.go
@@ -35,12 +35,18 @@ func (p Place) IsValid() bool {
 
 // Page is the representation of a page.
 type Page struct {
+	// Pattern is the string pattern which will be used to apply the page count component.
 	Pattern string
-	Place   Place
-	Family  string
-	Style   fontstyle.Type
-	Size    float64
-	Color   *Color
+	// Place defines where the page count component will be placed.
+	Place Place
+	// Family defines which font family will be applied to page count.
+	Family string
+	// Style defines which font style will be applied to page count.
+	Style fontstyle.Type
+	// Size defines which font size will be applied to page count.
+	Size float64
+	// Color defines which will be applied to page count.
+	Color *Color
 }
 
 // GetNumberTextProp returns the Text properties of the page number.
