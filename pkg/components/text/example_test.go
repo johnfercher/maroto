993fa10e09f00cc28c7c4c7f6fd65d0d22ff755c
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-06-05 21:35:02 -0300

    docs: update examples to include custom text

--- pkg/components/text/example_test.go
+++ pkg/components/text/example_test.go
@@ -3,16 +3,33 @@ package text_test
 import (
 	"github.com/johnfercher/maroto/v2"
 	"github.com/johnfercher/maroto/v2/pkg/components/col"
+	"github.com/johnfercher/maroto/v2/pkg/components/row"
 	"github.com/johnfercher/maroto/v2/pkg/components/text"
+	"github.com/johnfercher/maroto/v2/pkg/core/entity"
+	"github.com/johnfercher/maroto/v2/pkg/props"
 )
 
-// ExampleNew demonstrates how to create a text component.
+// ExampleNew demonstrates how to create a text components with different styles.
 func ExampleNew() {
 	m := maroto.New()
 
 	text := text.New("text")
 	col := col.New(12).Add(text)
 	m.AddRow(10, col)
+
+	// generate document
+}
+
+// ExampleNew demonstrates how to create a text component with
+func ExampleNewCustomText() {
+	m := maroto.New()
+
+	subText1 := entity.NewSubText("This is a text", props.SubText{Color: &props.BlueColor})
+	subText2 := entity.NewSubText(" with multiple", props.SubText{Size: 7})
+	subText3 := entity.NewSubText(" styles", props.SubText{Color: &props.RedColor})
+
+	customText := col.New(12).Add(text.NewCustomText([]*entity.SubText{subText1, subText2, subText3}))
+	m.AddRows(row.New(10).Add(customText))
 
 	// generate document
 }
