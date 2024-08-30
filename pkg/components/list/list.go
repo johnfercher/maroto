3324f842733364ae51eb6e7e8d55e37307420bf2
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-08-29 22:53:08 -0300

    feature:Implement new interface for list component
    Make list creation easier

--- pkg/components/list/list.go
+++ pkg/components/list/list.go
@@ -2,53 +2,34 @@
 package list
 
 import (
-	"errors"
-
+	"github.com/johnfercher/maroto/v2/pkg/components/row"
 	"github.com/johnfercher/maroto/v2/pkg/core"
 )
 
-// Listable is the main abstraction of a listable item in a TableList.
-// A collection of objects that implements this interface may be added
-// in a list.
-type Listable interface {
-	GetHeader() core.Row
-	GetContent(i int) core.Row
+type List struct {
+	Header  core.Row
+	Content []core.Row
 }
 
-// BuildFromPointer is responsible to receive a collection of objects that implements
-// Listable and build the rows of TableList. This method should be used in case of a collection
-// of pointers.
-func BuildFromPointer[T Listable](arr []*T) ([]core.Row, error) {
-	if len(arr) == 0 {
-		return nil, errors.New("empty array")
-	}
-
-	var list []T
-	for _, pointer := range arr {
-		if pointer == nil {
-			return nil, errors.New("nil element in array")
-		}
-		list = append(list, *pointer)
-	}
-
-	return Build(list)
+// Add is responsible for adding a row to the table content.
+func (l *List) Add(rows ...core.Row) *List {
+	l.Content = append(l.Content, rows...)
+	return l
 }
 
-// Build is responsible to receive a collection of objects that implements
-// Listable and build the rows of TableList. This method should be used in case of a collection
-// of values.
-func Build[T Listable](arr []T) ([]core.Row, error) {
-	if len(arr) == 0 {
-		return nil, errors.New("empty array")
+// GetRows is responsible for returning all rows in the table
+func (l *List) GetRows() []core.Row {
+	return append([]core.Row{l.Header}, l.Content...)
+}
+
+// New is responsible to create an instance of a List.
+func New(header core.Row) *List {
+	if header == nil {
+		header = row.New(0)
 	}
 
-	var rows []core.Row
-
-	rows = append(rows, arr[0].GetHeader())
-
-	for i, element := range arr {
-		rows = append(rows, element.GetContent(i))
+	return &List{
+		Header:  header,
+		Content: make([]core.Row, 0),
 	}
-
-	return rows, nil
 }
