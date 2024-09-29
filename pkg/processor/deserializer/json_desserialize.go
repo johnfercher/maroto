f8ffc58af15b75f6e5d9ab3bd4cfb58c76269509
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-09-29 10:43:58 -0300

    fix: update json deserializer to use new pdf struct

--- pkg/processor/deserializer/json_desserialize.go
+++ pkg/processor/deserializer/json_desserialize.go
@@ -3,9 +3,9 @@ package deserializer
 
 import (
 	"encoding/json"
-	"html/template"
 
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/content"
+	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/pdf"
 )
 
 type jsonDeserializer struct {
@@ -16,8 +16,8 @@ func NewJsonDeserialize() *jsonDeserializer {
 }
 
 // DesserializeTemplate is responsible for transforming a string into a template structure
-func (j *jsonDeserializer) DesserializeTemplate(templateJson string) (template.Template, error) {
-	return deserializer[template.Template](templateJson)
+func (j *jsonDeserializer) DesserializeTemplate(templateJson string) (pdf.Pdf, error) {
+	return deserializer[pdf.Pdf](templateJson)
 }
 
 // DesserializeContent is responsible for transforming a string into a content structure
