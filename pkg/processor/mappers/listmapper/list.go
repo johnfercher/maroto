7e5d15350164fc50f4011c192e8f339228226250
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-10-12 11:48:29 -0300

    feat: Implement component list creation
    The list should allow applying list behavior to any component

--- pkg/processor/mappers/listmapper/list.go
+++ pkg/processor/mappers/listmapper/list.go
@@ -2,17 +2,45 @@
 package listmapper
 
 import (
+	"fmt"
+
 	"github.com/johnfercher/maroto/v2/pkg/processor/components"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers"
 )
 
 type List struct {
-	sourceKey string
-	templates mappers.Componentmapper
+	SourceKey string
+	Templates []mappers.Componentmapper
 }
 
-func NewList(document interface{}, sourceKey string, generate mappers.GenerateComponent) (*List, error) {
-	return &List{}, nil
+func NewList(list interface{}, sourceKey string, generate mappers.GenerateComponent) (*List, error) {
+	listMapper, ok := list.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("ensure list can be converted to map[string] interface{}")
+	}
+
+	components, err := createComponents(listMapper, generate)
+	if err != nil {
+		return nil, err
+	}
+	return &List{
+		Templates: components,
+		SourceKey: sourceKey,
+	}, nil
+}
+
+func createComponents(listMapper map[string]interface{}, generate mappers.GenerateComponent) ([]mappers.Componentmapper, error) {
+	components := make([]mappers.Componentmapper, len(listMapper))
+	cont := 0
+	for templateName, template := range listMapper {
+		component, err := generate(template, templateName)
+		if err != nil {
+			return nil, err
+		}
+		components[cont] = component
+		cont++
+	}
+	return components, nil
 }
 
 func (r *List) Generate(content map[string]interface{}) (components.PdfComponent, error) {
