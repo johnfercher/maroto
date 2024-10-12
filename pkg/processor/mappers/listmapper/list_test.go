8ca8f1517a65efd3c4b8d827c7d27767b5f7cbdf
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-10-12 11:47:22 -0300

    test: validate list creation

--- pkg/processor/mappers/listmapper/list_test.go
+++ pkg/processor/mappers/listmapper/list_test.go
@@ -1,1 +1,58 @@
-package listmapper
+package listmapper_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/johnfercher/maroto/v2/internal/fixture"
+	"github.com/johnfercher/maroto/v2/mocks"
+	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/listmapper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+//se interface invalida for passada erro é retornada
+// se componente não puder ser gerado, erro é retornado
+// se uma lista de 2 componentes for passada, 1 erro é retornado
+
+func TestNewList(t *testing.T) {
+	t.Run("when invalid interface is sent, it should return an error", func(t *testing.T) {
+		var invalidInterface interface{} = 1
+		factory := mocks.NewAbstractFactoryMaps(t)
+
+		doc, err := listmapper.NewList(invalidInterface, "test", factory.NewPage)
+
+		assert.Nil(t, doc)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("when component not can generate, it should return an error", func(t *testing.T) {
+		templatePages := map[string]interface{}{
+			"page_template_1": nil,
+			"page_template_2": nil,
+		}
+
+		factory := mocks.NewAbstractFactoryMaps(t)
+		factory.On("NewPage", mock.Anything, "page_template_1").Return(nil, errors.New(""))
+		doc, err := listmapper.NewList(templatePages, "test", factory.NewPage)
+
+		assert.Nil(t, doc)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("when 2-components are sent, it should add 2 componentes in list", func(t *testing.T) {
+		validPage := fixture.MapperPage()
+		templatePages := map[string]interface{}{
+			"page_template_1": nil,
+			"page_template_2": nil,
+		}
+		factory := mocks.NewAbstractFactoryMaps(t)
+		factory.On("NewPage", mock.Anything, "page_template_1").Return(validPage, nil)
+		factory.On("NewPage", mock.Anything, "page_template_2").Return(validPage, nil)
+
+		doc, err := listmapper.NewList(templatePages, "test", factory.NewPage)
+
+		assert.Nil(t, err)
+		assert.Equal(t, len(doc.Templates), 2)
+	})
+}
