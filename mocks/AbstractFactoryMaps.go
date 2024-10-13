4d753474d83b62834adb97a7f4d8b341cfffd804
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-10-13 16:48:12 -0300

    feat: Implement component col creation

--- mocks/AbstractFactoryMaps.go
+++ mocks/AbstractFactoryMaps.go
@@ -20,6 +20,64 @@ func (_m *AbstractFactoryMaps) EXPECT() *AbstractFactoryMaps_Expecter {
 	return &AbstractFactoryMaps_Expecter{mock: &_m.Mock}
 }
 
+// NewBarcode provides a mock function with given fields: document
+func (_m *AbstractFactoryMaps) NewBarcode(document interface{}) (mappers.Componentmapper, error) {
+	ret := _m.Called(document)
+
+	if len(ret) == 0 {
+		panic("no return value specified for NewBarcode")
+	}
+
+	var r0 mappers.Componentmapper
+	var r1 error
+	if rf, ok := ret.Get(0).(func(interface{}) (mappers.Componentmapper, error)); ok {
+		return rf(document)
+	}
+	if rf, ok := ret.Get(0).(func(interface{}) mappers.Componentmapper); ok {
+		r0 = rf(document)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(mappers.Componentmapper)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(interface{}) error); ok {
+		r1 = rf(document)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AbstractFactoryMaps_NewBarcode_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'NewBarcode'
+type AbstractFactoryMaps_NewBarcode_Call struct {
+	*mock.Call
+}
+
+// NewBarcode is a helper method to define mock.On call
+//   - document interface{}
+func (_e *AbstractFactoryMaps_Expecter) NewBarcode(document interface{}) *AbstractFactoryMaps_NewBarcode_Call {
+	return &AbstractFactoryMaps_NewBarcode_Call{Call: _e.mock.On("NewBarcode", document)}
+}
+
+func (_c *AbstractFactoryMaps_NewBarcode_Call) Run(run func(document interface{})) *AbstractFactoryMaps_NewBarcode_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(interface{}))
+	})
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewBarcode_Call) Return(_a0 mappers.Componentmapper, _a1 error) *AbstractFactoryMaps_NewBarcode_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewBarcode_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewBarcode_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewCol provides a mock function with given fields: document
 func (_m *AbstractFactoryMaps) NewCol(document interface{}) (mappers.Componentmapper, error) {
 	ret := _m.Called(document)
@@ -74,6 +132,122 @@ func (_c *AbstractFactoryMaps_NewCol_Call) Return(_a0 mappers.Componentmapper, _
 }
 
 func (_c *AbstractFactoryMaps_NewCol_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewCol_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// NewImage provides a mock function with given fields: document
+func (_m *AbstractFactoryMaps) NewImage(document interface{}) (mappers.Componentmapper, error) {
+	ret := _m.Called(document)
+
+	if len(ret) == 0 {
+		panic("no return value specified for NewImage")
+	}
+
+	var r0 mappers.Componentmapper
+	var r1 error
+	if rf, ok := ret.Get(0).(func(interface{}) (mappers.Componentmapper, error)); ok {
+		return rf(document)
+	}
+	if rf, ok := ret.Get(0).(func(interface{}) mappers.Componentmapper); ok {
+		r0 = rf(document)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(mappers.Componentmapper)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(interface{}) error); ok {
+		r1 = rf(document)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AbstractFactoryMaps_NewImage_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'NewImage'
+type AbstractFactoryMaps_NewImage_Call struct {
+	*mock.Call
+}
+
+// NewImage is a helper method to define mock.On call
+//   - document interface{}
+func (_e *AbstractFactoryMaps_Expecter) NewImage(document interface{}) *AbstractFactoryMaps_NewImage_Call {
+	return &AbstractFactoryMaps_NewImage_Call{Call: _e.mock.On("NewImage", document)}
+}
+
+func (_c *AbstractFactoryMaps_NewImage_Call) Run(run func(document interface{})) *AbstractFactoryMaps_NewImage_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(interface{}))
+	})
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewImage_Call) Return(_a0 mappers.Componentmapper, _a1 error) *AbstractFactoryMaps_NewImage_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewImage_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewImage_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// NewLine provides a mock function with given fields: document
+func (_m *AbstractFactoryMaps) NewLine(document interface{}) (mappers.Componentmapper, error) {
+	ret := _m.Called(document)
+
+	if len(ret) == 0 {
+		panic("no return value specified for NewLine")
+	}
+
+	var r0 mappers.Componentmapper
+	var r1 error
+	if rf, ok := ret.Get(0).(func(interface{}) (mappers.Componentmapper, error)); ok {
+		return rf(document)
+	}
+	if rf, ok := ret.Get(0).(func(interface{}) mappers.Componentmapper); ok {
+		r0 = rf(document)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(mappers.Componentmapper)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(interface{}) error); ok {
+		r1 = rf(document)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AbstractFactoryMaps_NewLine_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'NewLine'
+type AbstractFactoryMaps_NewLine_Call struct {
+	*mock.Call
+}
+
+// NewLine is a helper method to define mock.On call
+//   - document interface{}
+func (_e *AbstractFactoryMaps_Expecter) NewLine(document interface{}) *AbstractFactoryMaps_NewLine_Call {
+	return &AbstractFactoryMaps_NewLine_Call{Call: _e.mock.On("NewLine", document)}
+}
+
+func (_c *AbstractFactoryMaps_NewLine_Call) Run(run func(document interface{})) *AbstractFactoryMaps_NewLine_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(interface{}))
+	})
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewLine_Call) Return(_a0 mappers.Componentmapper, _a1 error) *AbstractFactoryMaps_NewLine_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewLine_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewLine_Call {
 	_c.Call.Return(run)
 	return _c
 }
@@ -138,6 +312,64 @@ func (_c *AbstractFactoryMaps_NewList_Call) RunAndReturn(run func(interface{}, s
 	return _c
 }
 
+// NewMatrixcode provides a mock function with given fields: document
+func (_m *AbstractFactoryMaps) NewMatrixcode(document interface{}) (mappers.Componentmapper, error) {
+	ret := _m.Called(document)
+
+	if len(ret) == 0 {
+		panic("no return value specified for NewMatrixcode")
+	}
+
+	var r0 mappers.Componentmapper
+	var r1 error
+	if rf, ok := ret.Get(0).(func(interface{}) (mappers.Componentmapper, error)); ok {
+		return rf(document)
+	}
+	if rf, ok := ret.Get(0).(func(interface{}) mappers.Componentmapper); ok {
+		r0 = rf(document)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(mappers.Componentmapper)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(interface{}) error); ok {
+		r1 = rf(document)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AbstractFactoryMaps_NewMatrixcode_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'NewMatrixcode'
+type AbstractFactoryMaps_NewMatrixcode_Call struct {
+	*mock.Call
+}
+
+// NewMatrixcode is a helper method to define mock.On call
+//   - document interface{}
+func (_e *AbstractFactoryMaps_Expecter) NewMatrixcode(document interface{}) *AbstractFactoryMaps_NewMatrixcode_Call {
+	return &AbstractFactoryMaps_NewMatrixcode_Call{Call: _e.mock.On("NewMatrixcode", document)}
+}
+
+func (_c *AbstractFactoryMaps_NewMatrixcode_Call) Run(run func(document interface{})) *AbstractFactoryMaps_NewMatrixcode_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(interface{}))
+	})
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewMatrixcode_Call) Return(_a0 mappers.Componentmapper, _a1 error) *AbstractFactoryMaps_NewMatrixcode_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewMatrixcode_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewMatrixcode_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewPage provides a mock function with given fields: document, sourceKey
 func (_m *AbstractFactoryMaps) NewPage(document interface{}, sourceKey string) (mappers.Componentmapper, error) {
 	ret := _m.Called(document, sourceKey)
@@ -197,6 +429,64 @@ func (_c *AbstractFactoryMaps_NewPage_Call) RunAndReturn(run func(interface{}, s
 	return _c
 }
 
+// NewQrcode provides a mock function with given fields: document
+func (_m *AbstractFactoryMaps) NewQrcode(document interface{}) (mappers.Componentmapper, error) {
+	ret := _m.Called(document)
+
+	if len(ret) == 0 {
+		panic("no return value specified for NewQrcode")
+	}
+
+	var r0 mappers.Componentmapper
+	var r1 error
+	if rf, ok := ret.Get(0).(func(interface{}) (mappers.Componentmapper, error)); ok {
+		return rf(document)
+	}
+	if rf, ok := ret.Get(0).(func(interface{}) mappers.Componentmapper); ok {
+		r0 = rf(document)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(mappers.Componentmapper)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(interface{}) error); ok {
+		r1 = rf(document)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AbstractFactoryMaps_NewQrcode_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'NewQrcode'
+type AbstractFactoryMaps_NewQrcode_Call struct {
+	*mock.Call
+}
+
+// NewQrcode is a helper method to define mock.On call
+//   - document interface{}
+func (_e *AbstractFactoryMaps_Expecter) NewQrcode(document interface{}) *AbstractFactoryMaps_NewQrcode_Call {
+	return &AbstractFactoryMaps_NewQrcode_Call{Call: _e.mock.On("NewQrcode", document)}
+}
+
+func (_c *AbstractFactoryMaps_NewQrcode_Call) Run(run func(document interface{})) *AbstractFactoryMaps_NewQrcode_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(interface{}))
+	})
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewQrcode_Call) Return(_a0 mappers.Componentmapper, _a1 error) *AbstractFactoryMaps_NewQrcode_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewQrcode_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewQrcode_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewRow provides a mock function with given fields: document, sourceKey
 func (_m *AbstractFactoryMaps) NewRow(document interface{}, sourceKey string) (mappers.Componentmapper, error) {
 	ret := _m.Called(document, sourceKey)
@@ -252,6 +542,122 @@ func (_c *AbstractFactoryMaps_NewRow_Call) Return(_a0 mappers.Componentmapper, _
 }
 
 func (_c *AbstractFactoryMaps_NewRow_Call) RunAndReturn(run func(interface{}, string) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewRow_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// NewSignature provides a mock function with given fields: document
+func (_m *AbstractFactoryMaps) NewSignature(document interface{}) (mappers.Componentmapper, error) {
+	ret := _m.Called(document)
+
+	if len(ret) == 0 {
+		panic("no return value specified for NewSignature")
+	}
+
+	var r0 mappers.Componentmapper
+	var r1 error
+	if rf, ok := ret.Get(0).(func(interface{}) (mappers.Componentmapper, error)); ok {
+		return rf(document)
+	}
+	if rf, ok := ret.Get(0).(func(interface{}) mappers.Componentmapper); ok {
+		r0 = rf(document)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(mappers.Componentmapper)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(interface{}) error); ok {
+		r1 = rf(document)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AbstractFactoryMaps_NewSignature_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'NewSignature'
+type AbstractFactoryMaps_NewSignature_Call struct {
+	*mock.Call
+}
+
+// NewSignature is a helper method to define mock.On call
+//   - document interface{}
+func (_e *AbstractFactoryMaps_Expecter) NewSignature(document interface{}) *AbstractFactoryMaps_NewSignature_Call {
+	return &AbstractFactoryMaps_NewSignature_Call{Call: _e.mock.On("NewSignature", document)}
+}
+
+func (_c *AbstractFactoryMaps_NewSignature_Call) Run(run func(document interface{})) *AbstractFactoryMaps_NewSignature_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(interface{}))
+	})
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewSignature_Call) Return(_a0 mappers.Componentmapper, _a1 error) *AbstractFactoryMaps_NewSignature_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewSignature_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewSignature_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// NewText provides a mock function with given fields: document
+func (_m *AbstractFactoryMaps) NewText(document interface{}) (mappers.Componentmapper, error) {
+	ret := _m.Called(document)
+
+	if len(ret) == 0 {
+		panic("no return value specified for NewText")
+	}
+
+	var r0 mappers.Componentmapper
+	var r1 error
+	if rf, ok := ret.Get(0).(func(interface{}) (mappers.Componentmapper, error)); ok {
+		return rf(document)
+	}
+	if rf, ok := ret.Get(0).(func(interface{}) mappers.Componentmapper); ok {
+		r0 = rf(document)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(mappers.Componentmapper)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(interface{}) error); ok {
+		r1 = rf(document)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AbstractFactoryMaps_NewText_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'NewText'
+type AbstractFactoryMaps_NewText_Call struct {
+	*mock.Call
+}
+
+// NewText is a helper method to define mock.On call
+//   - document interface{}
+func (_e *AbstractFactoryMaps_Expecter) NewText(document interface{}) *AbstractFactoryMaps_NewText_Call {
+	return &AbstractFactoryMaps_NewText_Call{Call: _e.mock.On("NewText", document)}
+}
+
+func (_c *AbstractFactoryMaps_NewText_Call) Run(run func(document interface{})) *AbstractFactoryMaps_NewText_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(interface{}))
+	})
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewText_Call) Return(_a0 mappers.Componentmapper, _a1 error) *AbstractFactoryMaps_NewText_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AbstractFactoryMaps_NewText_Call) RunAndReturn(run func(interface{}) (mappers.Componentmapper, error)) *AbstractFactoryMaps_NewText_Call {
 	_c.Call.Return(run)
 	return _c
 }
--- pkg/processor/mappers/components/colmapper/col.go
+++ pkg/processor/mappers/components/colmapper/col.go
@@ -1,19 +1,80 @@
 package colmapper
 
 import (
+	"fmt"
+
 	"github.com/johnfercher/maroto/v2/pkg/processor/components"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers"
 )
 
 type Col struct {
 	Size       int
-	Components mappers.Componentmapper
+	Components []mappers.Componentmapper
+	factory    mappers.AbstractFactoryMaps
 }
 
-func NewCol(interface{}) (*Col, error) {
-	return nil, nil
+func NewCol(templateCols interface{}, factory mappers.AbstractFactoryMaps) (*Col, error) {
+	mapCols, ok := templateCols.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("ensure that rows can be converted to map[string] interface{}")
+	}
+
+	col := &Col{Size: 0, Components: make([]mappers.Componentmapper, 0), factory: factory}
+
+	if err := col.setSize(&mapCols); err != nil {
+		return nil, err
+	}
+
+	if err := col.addComponents(mapCols); err != nil {
+		return nil, err
+	}
+	return col, nil
 }
 
 func (c *Col) Generate(content map[string]interface{}) (components.PdfComponent, error) {
 	return nil, nil
+}
+
+func (c *Col) addComponents(mapComponents map[string]interface{}) error {
+	fieldMappers := c.getFieldMappers()
+
+	for templateName, template := range mapComponents {
+		mapper, ok := fieldMappers[templateName]
+		if !ok {
+			return fmt.Errorf("the field %s present in the col cannot be mapped to any valid component", templateName)
+		}
+		component, err := mapper(template)
+		if err != nil {
+			return err
+		}
+		c.Components = append(c.Components, component)
+	}
+	return nil
+}
+
+func (c *Col) setSize(template *map[string]interface{}) error {
+	defer delete(*template, "size")
+	templateSize, ok := (*template)["size"]
+	if ok {
+		size, ok := templateSize.(float64)
+		if !ok {
+			return fmt.Errorf("ensure that size can be converted to int")
+		}
+		c.Size = int(size)
+	}
+	return nil
+}
+
+// getFieldMappers is responsible for defining which methods are responsible for assembling which components.
+// To do this, the component name is linked to a function in a Map.
+func (c *Col) getFieldMappers() map[string]func(interface{}) (mappers.Componentmapper, error) {
+	return map[string]func(interface{}) (mappers.Componentmapper, error){
+		"bar_code":    c.factory.NewBarcode,
+		"matrix_code": c.factory.NewMatrixcode,
+		"qr_code":     c.factory.NewQrcode,
+		"image":       c.factory.NewImage,
+		"line":        c.factory.NewLine,
+		"signature":   c.factory.NewSignature,
+		"text":        c.factory.NewText,
+	}
 }
