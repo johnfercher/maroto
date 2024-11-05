6613097d04cb27262fe982004171fdd0caeabbd1
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-11-04 22:32:01 -0300

    test: validate page creation

--- pkg/processor/mappers/components/pagemapper/page_test.go
+++ pkg/processor/mappers/components/pagemapper/page_test.go
@@ -1,10 +1,12 @@
 package pagemapper_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/johnfercher/maroto/v2/internal/fixture"
 	"github.com/johnfercher/maroto/v2/mocks"
+	"github.com/johnfercher/maroto/v2/pkg/processor/mappers"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/listmapper"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/pagemapper"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/rowmapper"
@@ -55,5 +57,53 @@ func TestNewPage(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, len(doc.Rows), 1)
 		assert.IsType(t, &listmapper.List{}, doc.Rows[0])
+	})
+}
+
+func TestGenerate(t *testing.T) {
+	t.Run("when content no has source_key, should return an error", func(t *testing.T) {
+		content := map[string]interface{}{}
+		factory := mocks.NewAbstractFactoryMaps(t)
+		provider := mocks.NewProcessorProvider(t)
+
+		page := pagemapper.Page{Rows: make([]mappers.Componentmapper, 0), Factory: factory, SourceKey: "test"}
+		newPage, err := page.Generate(content, provider)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, newPage)
+	})
+	t.Run("when page no has rows, it should no sent rows", func(t *testing.T) {
+		content := map[string]interface{}{"content": map[string]interface{}{"row_1": "any"}}
+		factory := mocks.NewAbstractFactoryMaps(t)
+		provider := mocks.NewProcessorProvider(t)
+		provider.EXPECT().CreatePage().Return(nil, nil)
+
+		page := pagemapper.Page{Rows: make([]mappers.Componentmapper, 0), Factory: factory, SourceKey: "content"}
+		_, err := page.Generate(content, provider)
+
+		assert.Nil(t, err)
+	})
+	t.Run("when is not possible generate components, should return an error", func(t *testing.T) {
+		content := map[string]interface{}{"content": map[string]interface{}{"text": "value"}}
+		factory := mocks.NewAbstractFactoryMaps(t)
+		provider := mocks.NewProcessorProvider(t)
+		component := mocks.NewComponentmapper(t)
+		component.EXPECT().Generate(mock.Anything, provider).Return(nil, fmt.Errorf("any"))
+
+		page := pagemapper.Page{Rows: []mappers.Componentmapper{component}, Factory: factory, SourceKey: "content"}
+		_, err := page.Generate(content, provider)
+
+		assert.NotNil(t, err)
+	})
+	t.Run("when is not possible generate page, should return an error", func(t *testing.T) {
+		content := map[string]interface{}{"content": map[string]interface{}{"text": "value"}}
+		factory := mocks.NewAbstractFactoryMaps(t)
+		provider := mocks.NewProcessorProvider(t)
+		provider.EXPECT().CreatePage().Return(nil, fmt.Errorf("any"))
+
+		page := pagemapper.Page{Rows: make([]mappers.Componentmapper, 0), Factory: factory, SourceKey: "content"}
+		_, err := page.Generate(content, provider)
+
+		assert.NotNil(t, err)
 	})
 }
