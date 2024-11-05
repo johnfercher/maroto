9471b8968cf54203dd43c6d25ab3145285643f11
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-11-04 23:16:09 -0300

    test: Validate component list creation

--- pkg/processor/mappers/components/listmapper/list_test.go
+++ pkg/processor/mappers/components/listmapper/list_test.go
@@ -2,11 +2,14 @@ package listmapper_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/johnfercher/maroto/v2/internal/fixture"
 	"github.com/johnfercher/maroto/v2/mocks"
+	"github.com/johnfercher/maroto/v2/pkg/processor/mappers"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/listmapper"
+	"github.com/johnfercher/maroto/v2/pkg/processor/processorprovider"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -50,5 +53,60 @@ func TestNewList(t *testing.T) {
 
 		assert.Nil(t, err)
 		assert.Equal(t, len(doc.Templates), 2)
+	})
+}
+
+func TestGenerate(t *testing.T) {
+	t.Run("when source_key is not found, should return an error", func(t *testing.T) {
+		provider := mocks.NewProcessorProvider(t)
+		content := map[string]interface{}{}
+		list := listmapper.List{SourceKey: "list", Templates: make([]mappers.Componentmapper, 0)}
+
+		components, err := list.Generate(content, provider)
+
+		assert.Nil(t, components)
+		assert.NotNil(t, err)
+	})
+	t.Run("when invalid content is sent, should return an error", func(t *testing.T) {
+		provider := mocks.NewProcessorProvider(t)
+		content := map[string]interface{}{"list": 1}
+		list := listmapper.List{SourceKey: "list", Templates: make([]mappers.Componentmapper, 0)}
+
+		components, err := list.Generate(content, provider)
+
+		assert.Nil(t, components)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("when componentes is not generate, should return an error", func(t *testing.T) {
+		contentRow1 := map[string]interface{}{"row_1": nil}
+		listContent := map[string]interface{}{"list": []map[string]interface{}{contentRow1}}
+		provider := mocks.NewProcessorProvider(t)
+		component := mocks.NewComponentmapper(t)
+		component.EXPECT().Generate(mock.Anything, provider).Return(nil, fmt.Errorf("any"))
+
+		list := listmapper.List{SourceKey: "list", Templates: []mappers.Componentmapper{component}}
+		components, err := list.Generate(listContent, provider)
+
+		assert.Nil(t, components)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("when 2 templates are added, it should generate 4 components", func(t *testing.T) {
+		content1 := map[string]interface{}{"row_1": nil, "row_2": nil}
+		content2 := map[string]interface{}{"row_1": nil, "row_2": nil}
+		listContent := map[string]interface{}{"list": []map[string]interface{}{content1, content2}}
+		provider := mocks.NewProcessorProvider(t)
+		providerComponent := mocks.NewProviderComponent(t)
+		component := mocks.NewComponentmapper(t)
+		component.EXPECT().Generate(mock.Anything, provider).Return([]processorprovider.ProviderComponent{providerComponent}, nil)
+
+		list := listmapper.List{SourceKey: "list", Templates: []mappers.Componentmapper{component, component}}
+		components, err := list.Generate(listContent, provider)
+
+		assert.NotNil(t, components)
+		assert.Nil(t, err)
+		component.AssertNumberOfCalls(t, "Generate", 4)
+		assert.Len(t, components, 4)
 	})
 }
