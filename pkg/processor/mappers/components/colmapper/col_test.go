42c6f58ffe6cf0c5ea70042a141d68832209c5de
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-11-04 22:32:46 -0300

    test: validate col creation

--- pkg/processor/mappers/components/colmapper/col_test.go
+++ pkg/processor/mappers/components/colmapper/col_test.go
@@ -6,12 +6,15 @@ import (
 
 	"github.com/johnfercher/maroto/v2/internal/fixture"
 	"github.com/johnfercher/maroto/v2/mocks"
+	"github.com/johnfercher/maroto/v2/pkg/components/text"
+	"github.com/johnfercher/maroto/v2/pkg/processor/mappers"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/codemapper"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/colmapper"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/imagemapper"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/linemapper"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/signaturemapper"
 	"github.com/johnfercher/maroto/v2/pkg/processor/mappers/components/textmapper"
+	"github.com/johnfercher/maroto/v2/pkg/processor/processorprovider"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -164,5 +167,47 @@ func TestNewCol(t *testing.T) {
 
 		assert.Nil(t, err)
 		assert.Equal(t, doc.Size, 6)
+	})
+}
+
+func TestGenerate(t *testing.T) {
+	t.Run("when col has no components, it should not send components", func(t *testing.T) {
+		content := map[string]interface{}{}
+		col := colmapper.Col{Size: 10, Components: make([]mappers.Componentmapper, 0)}
+		provider := mocks.NewProcessorProvider(t)
+		provider.EXPECT().CreateCol(10).Return(nil, nil)
+
+		_, err := col.Generate(content, provider)
+
+		assert.Nil(t, err)
+		provider.AssertNumberOfCalls(t, "CreateCol", 1)
+	})
+	t.Run("when col has two components, it should add two components", func(t *testing.T) {
+		content := map[string]interface{}{"text1": "test", "text2": "test"}
+		provider := mocks.NewProcessorProvider(t)
+		provider.EXPECT().CreateCol(10, text.New("test"), text.New("test")).Return(nil, nil)
+
+		component := mocks.NewComponentmapper(t)
+		component.EXPECT().Generate(content, provider).Return([]processorprovider.ProviderComponent{text.New("test")}, nil)
+		col := colmapper.Col{Size: 10, Components: []mappers.Componentmapper{component, component}}
+
+		_, err := col.Generate(content, provider)
+
+		assert.Nil(t, err)
+		provider.AssertNumberOfCalls(t, "CreateCol", 1)
+		component.AssertNumberOfCalls(t, "Generate", 2)
+	})
+	t.Run("when it is not possible to generate the component, an error should be returned", func(t *testing.T) {
+		content := map[string]interface{}{"text1": "test"}
+		provider := mocks.NewProcessorProvider(t)
+
+		component := mocks.NewComponentmapper(t)
+		component.EXPECT().Generate(content, provider).Return(nil, errors.New("any"))
+		col := colmapper.Col{Size: 10, Components: []mappers.Componentmapper{component}}
+
+		_, err := col.Generate(content, provider)
+
+		component.AssertNumberOfCalls(t, "Generate", 1)
+		assert.NotNil(t, err)
 	})
 }
