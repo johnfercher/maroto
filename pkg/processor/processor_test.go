fc6cd298b5214933e3fa3075deb016687f23d611
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2025-01-01 18:54:22 -0300

    test: validate disableautopagebreak example build

--- pkg/processor/processor_test.go
+++ pkg/processor/processor_test.go
@@ -184,6 +184,19 @@ func TestGenerateTemplate(t *testing.T) {
 		assert.Nil(t, err)
 		test.New(t).Assert((*provider).GetStructure()).Equals("examples/datamatrixgrid.json")
 	})
+	t.Run("when template with autopagebreak example is found, should set template", func(t *testing.T) {
+		test.SetupTestDir(t)
+
+		fixtemplate, _ := processortest.NewFileReader().LoadFile("processor/json/disablepagebreak_template.json")
+		processor := processor.NewProcessor(repository.NewMemoryStorage(loader.NewLoader()), deserializer.NewJSONDeserializer())
+		err := processor.RegisterTemplate("disablepagebreak", string(fixtemplate))
+		require.NoError(t, err)
+
+		provider, err := processor.GenerateDocument("disablepagebreak", "{}")
+
+		assert.Nil(t, err)
+		test.New(t).Assert((*provider).GetStructure()).Equals("examples/disablepagebreak.json")
+	})
 	t.Run("when template with invalid field is found, should return an error", func(t *testing.T) {
 
 	})
