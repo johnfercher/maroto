c4da9ddf9ca04053b0d00d1bd0319b747c6499c8
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2025-01-01 18:22:17 -0300

    test: validate custompage example build

--- pkg/processor/processor_test.go
+++ pkg/processor/processor_test.go
@@ -144,10 +144,35 @@ func TestGenerateTemplate(t *testing.T) {
 		assert.Nil(t, err)
 		test.New(t).Assert((*provider).GetStructure()).Equals("examples/customdimensions.json")
 	})
+	t.Run("when template with customfont example is found, should set template", func(t *testing.T) {
+		test.SetupTestDir(t)
+
+		fixtemplate, _ := processortest.NewFileReader().LoadFile("processor/json/customfont_template.json")
+		fixcontent, _ := processortest.NewFileReader().LoadFile("processor/json/customfont_content.json")
+		processor := processor.NewProcessor(repository.NewMemoryStorage(loader.NewLoader()), deserializer.NewJSONDeserializer())
+		err := processor.RegisterTemplate("font", string(fixtemplate))
+		require.NoError(t, err)
+
+		provider, err := processor.GenerateDocument("font", string(fixcontent))
+
+		assert.Nil(t, err)
+		test.New(t).Assert((*provider).GetStructure()).Equals("examples/customfont.json")
+	})
+	t.Run("when template with custompage example is found, should set template", func(t *testing.T) {
+		test.SetupTestDir(t)
+
+		fixtemplate, _ := processortest.NewFileReader().LoadFile("processor/json/custompage_template.json")
+		processor := processor.NewProcessor(repository.NewMemoryStorage(loader.NewLoader()), deserializer.NewJSONDeserializer())
+		err := processor.RegisterTemplate("page", string(fixtemplate))
+		require.NoError(t, err)
+
+		provider, err := processor.GenerateDocument("page", "{}")
+
+		assert.Nil(t, err)
+		test.New(t).Assert((*provider).GetStructure()).Equals("examples/custompage.json")
+	})
 	t.Run("when template with invalid field is found, should return an error", func(t *testing.T) {
-		// doc, _ := (*provider).Generate()
-		// err = doc.Save("test.pdf")
-		// assert.Nil(t, err)
+
 	})
 	t.Run("when invalid content is sent, should return an error", func(t *testing.T) {
 	})
