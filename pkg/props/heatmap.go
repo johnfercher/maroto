de5745bf4fd7795154e1c395db74c8ae0742a8c6
johnfercher <johnathanfercher22@gmail.com>
2025-02-13 22:38:51 -0300

    feature: heatmap

---
+++ pkg/props/heatmap.go
@@ -0,0 +1,25 @@
+package props
+
+type HeatMap struct {
+	TransparentValues []int
+	InvertScale       bool
+	Chart             *Chart
+}
+
+func (h *HeatMap) MakeValid() {
+
+}
+
+func (h *HeatMap) ToMap() map[string]any {
+	if h == nil {
+		return nil
+	}
+
+	m := make(map[string]interface{})
+
+	if len(h.TransparentValues) > 0 {
+		m["prop_transparent_values"] = h.TransparentValues
+	}
+
+	return m
+}
