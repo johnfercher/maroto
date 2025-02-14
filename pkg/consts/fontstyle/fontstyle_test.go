64835e121356168b16c7089b39edc232e214289e
Jonas Zeiger <jonas.zeiger@talpidae.net>
2025-02-14 01:51:59 +0100

    Add underline and strike-through fontstyle (#505)
    Co-authored-by: John Fercher <4752636+johnfercher@users.noreply.github.com>

--- pkg/consts/fontstyle/fontstyle_test.go
+++ pkg/consts/fontstyle/fontstyle_test.go
@@ -3,8 +3,9 @@ package fontstyle_test
 import (
 	"testing"
 
-	"github.com/johnfercher/maroto/v2/pkg/consts/fontstyle"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/johnfercher/maroto/v2/pkg/consts/fontstyle"
 )
 
 func TestType_IsValid(t *testing.T) {
@@ -32,6 +33,27 @@ func TestType_IsValid(t *testing.T) {
 	t.Run("when style is bold italic, should be valid", func(t *testing.T) {
 		// Arrange
 		fontStyle := fontstyle.BoldItalic
+
+		// Act & Assert
+		assert.True(t, fontStyle.IsValid())
+	})
+	t.Run("when style is underline, should be valid", func(t *testing.T) {
+		// Arrange
+		fontStyle := fontstyle.Underline
+
+		// Act & Assert
+		assert.True(t, fontStyle.IsValid())
+	})
+	t.Run("when style is strike-through, should be valid", func(t *testing.T) {
+		// Arrange
+		fontStyle := fontstyle.Strikethrough
+
+		// Act & Assert
+		assert.True(t, fontStyle.IsValid())
+	})
+	t.Run("when style is bold, strike-through and italic, should be valid", func(t *testing.T) {
+		// Arrange
+		fontStyle := fontstyle.Bold + fontstyle.Strikethrough + fontstyle.Italic
 
 		// Act & Assert
 		assert.True(t, fontStyle.IsValid())
